d5eb5edd16cca2927660c45791f249194d4b4335
h.toriyama <toriyamahiroto1128@gmail.com>
2023-05-03 09:50:58 +0900

    prisma導入

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express, {Request, Response} from 'express';
 import dotenv from 'dotenv';
+import {PrismaClient} from '@prisma/client';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
+const prisma = new PrismaClient();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.get('/', async (req: Request, res: Response) => {
+  const allUsers = await prisma.user.findMany();
+  res.send(allUsers);
 });
 
 app.listen(port, () => {
